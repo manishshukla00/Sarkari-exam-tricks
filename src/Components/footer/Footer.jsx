a3e4fb54dd6280daf56d8610a1f3094d21fc3cad
manishshukla00 <132373386+manishshukla00@users.noreply.github.com>
2025-03-23 10:15:00 +0530

    Add files via upload

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <footer>
-      <div className='pt-4 pb-4 text-center'>
-        <p className='border-t-2 border-b-2'><i class="fa-solid fa-magnifying-glass"></i>This site uses Google AdSense ad intent links. AdSense automatically generates these links and they may help creators earn money.</p>
+      <div className="pt-4 pb-4 text-center">
+        <p className="border-t-2 border-b-2">
+          <i class="fa-solid fa-magnifying-glass"></i>This site uses Google
+          AdSense ad intent links. AdSense automatically generates these links
+          and they may help creators earn money.
+        </p>
       </div>
 
-      <div className='mx-auto p-5 flex justify-around'>
+      <div className="mx-auto p-5 flex justify-around">
         {/* Quick Links */}
         <div className="flex flex-col">
           <h3 className="text-xl font-bold mb-4">Quick Links</h3>
           <ul className="space-y-2">
-            <li><a href="#" >Sarkari Result @X</a></li>
-            <li><a href="#" >Sarkari Result @Telegram</a></li>
-            <li><a href="#" >Sarkari Result @WhatsApp</a></li>
-            <li><a href="#" >Sarkari Result @Instagram</a></li>
-            <li><a href="#" >Sarkari Result @Threads</a></li>
-            <li><a href="#" >Sarkari Result @Facebook</a></li>
-            <li><a href="#" >Sarkari Result @Linkedin</a></li>
-            <li><a href="#" >Sarkari Result @Youtube</a></li>
-            <li><a href="#" >All Board Exams</a></li>
-            <li><a href="#" >Sarkari Result Contact Us</a></li>
-            <li><a href="#" >Privacy Policy</a></li>
+            <li>
+              <a href="#">Sarkari Exam Tricks @X</a>
+            </li>
+            <li>
+              <a href="#">Sarkari Exam Tricks @Telegram</a>
+            </li>
+            <li>
+              <a href="#">Sarkari Exam Tricks @WhatsApp</a>
+            </li>
+            <li>
+              <a href="#">Sarkari Exam Tricks @Instagram</a>
+            </li>
+            <li>
+              <a href="#">Sarkari Exam Tricks @Threads</a>
+            </li>
+            <li>
+              <a href="#">Sarkari Exam Tricks @Facebook</a>
+            </li>
+            <li>
+              <a href="#">Sarkari Exam Tricks @Linkedin</a>
+            </li>
+            <li>
+              <a href="#">Sarkari Exam Tricks @Youtube</a>
+            </li>
+            <li>
+              <a href="#">All Board Exams</a>
+            </li>
+            <li>
+              <a href="#">Sarkari Exam Tricks Contact Us</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
           </ul>
         </div>
 
@@ -30,8 +56,12 @@ const Footer = () => {
         <div className="flex flex-col">
           <h3 className="text-xl font-bold mb-4">Apps</h3>
           <ul className="space-y-2">
-            <li><a href="#" >Android App</a></li>
-            <li><a href="#" >Apple IOS App</a></li>
+            <li>
+              <a href="#">Android App</a>
+            </li>
+            <li>
+              <a href="#">Apple IOS App</a>
+            </li>
           </ul>
         </div>
 
@@ -39,29 +69,28 @@ const Footer = () => {
         <div className="flex flex-col">
           <h3 className="text-xl font-bold mb-4">More</h3>
           <div className="grid grid-cols-3 gap-2">
-            <a href="#" >BPSC</a>
-            <a href="#" >UPSSSC</a>
-            <a href="#" >CCC</a>
-            <a href="#" >UP-scholarship</a>
-            <a href="#" >IBPS</a>
-            <a href="#" >SSC</a>
-            <a href="#" >UPSC</a>
-            <a href="#" >Air Force</a>
-            <a href="#" >Navy</a>
-            <a href="#" >RPSC</a>
-            <a href="#" >Delhi Dssb</a>
-            <a href="#" >Railway</a>
-            <a href="#" >UPPCS</a>
+            <a href="#">BPSC</a>
+            <a href="#">UPSSSC</a>
+            <a href="#">CCC</a>
+            <a href="#">UP-scholarship</a>
+            <a href="#">IBPS</a>
+            <a href="#">SSC</a>
+            <a href="#">UPSC</a>
+            <a href="#">Air Force</a>
+            <a href="#">Navy</a>
+            <a href="#">RPSC</a>
+            <a href="#">Delhi Dssb</a>
+            <a href="#">Railway</a>
+            <a href="#">UPPCS</a>
             <a href="#">HSSC</a>
-            <a href="#" >TET</a>
-            <a href="#" >Coast Guard</a>
-            <a href="#" >Police Vacancy</a>
-          </div >
-        </div >
+            <a href="#">TET</a>
+            <a href="#">Coast Guard</a>
+            <a href="#">Police Vacancy</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-      </div >
-    </footer >
-  )
-}
-
-export default Footer
+export default Footer;
